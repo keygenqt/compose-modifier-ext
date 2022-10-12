bf6d5d836c82487f2697c0b44873ef4624d71857
Keygenqt <keygenqt@gmail.com>
2022-10-12 19:35:49 +0300

    Update android versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
-    id("com.jfrog.artifactory")
     id("com.diffplug.spotless")
 }
-
-val composeVersion: String = findProperty("composeVersion") as? String ?: "1.1.0-rc01"
 
 version = "0.0.13"
 group = "com.keygenqt.modifier"
@@ -18,43 +14,18 @@ spotless {
     }
 }
 
-publishing {
-    publications {
-        register("aar", MavenPublication::class) {
-            groupId = group.toString()
-            artifactId = project.name
-            artifact("$buildDir/outputs/aar/modifier-ext-$version-debug.aar")
-        }
-    }
-}
-
-artifactory {
-    setContextUrl("https://artifactory.keygenqt.com/artifactory")
-    publish {
-        repository {
-            setRepoKey("open-source")
-            setUsername(findProperty("arusername").toString())
-            setPassword(findProperty("arpassword").toString())
-        }
-        defaults {
-            publications("aar")
-            setPublishArtifacts(true)
-        }
-    }
-}
-
 android {
 
-    compileSdk = 30
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 31
+        targetSdk = 33
         setProperty("archivesBaseName", "modifier-ext-$version")
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = findProperty("composeCompilerVersion").toString()
     }
 
     buildFeatures {
@@ -63,6 +34,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui:1.1.0-rc01")
+    implementation("androidx.compose.material:material:1.1.0-rc01")
 }
